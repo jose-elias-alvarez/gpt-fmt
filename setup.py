b32a5011dd55a2d82ce69124cb2b34bf6066a976
Jose Alvarez <jealv@icloud.com>
2024-09-30 21:37:47 -0400

    chore: define requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup  # pyright: ignore[reportUnknownVariableType]
 setup(
     name="gpt-fmt",
     version="0.0.1",
-    install_requires=["openai>=1.41.0"],
+    install_requires=["openai>=1.50.2"],
     package_dir={"": "src"},
     entry_points={
         "console_scripts": [
